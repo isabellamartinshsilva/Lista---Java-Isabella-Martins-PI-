9aa51d9de1673586a49d10bf1353c1c190987da8
Isabella Martins <isabellamartinshsilva@gmail.com>
2026-04-19 09:14:19 -0300

    Q9.java

---
+++ Q9.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Q9 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] matriz = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        int somaPrincipal = 0;
+        int somaSecundaria = 0;
+        int somaTotal = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i == j) {
+                    somaPrincipal += matriz[i][j];
+                    somaTotal += matriz[i][j];
+                }
+
+                if (i + j == n - 1) {
+                    somaSecundaria += matriz[i][j];
+
+                    // evita repetir o elemento do meio
+                    if (i != j) {
+                        somaTotal += matriz[i][j];
+                    }
+                }
+            }
+        }
+
+        System.out.println("Soma da diagonal principal: " + somaPrincipal);
+        System.out.println("Soma da diagonal secundaria: " + somaSecundaria);
+        System.out.println("Soma total das diagonais sem repetir o centro: " + somaTotal);
+
+        sc.close();
+    }
+}
