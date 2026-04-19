cff8c9ec1d0746c2a1adfa0f6e0b8f58bb96bde1
Isabella Martins <isabellamartinshsilva@gmail.com>
2026-04-19 08:37:07 -0300

    Create Q1.java

---
+++ Q1.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Q1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] consumo = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            consumo[i] = sc.nextInt();
+        }
+
+        int total = calcularTotal(consumo);
+        double media = calcularMedia(total, n);
+        int acimaMedia = contarAcimaMedia(consumo, media);
+
+        System.out.println("Consumo total: " + total);
+        System.out.println("Consumo medio: " + media);
+        System.out.println("Dias acima da media: " + acimaMedia);
+
+        sc.close();
+    }
+
+    public static int calcularTotal(int[] vetor) {
+        int soma = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            soma += vetor[i];
+        }
+        return soma;
+    }
+
+    public static double calcularMedia(int total, int quantidade) {
+        return (double) total / quantidade;
+    }
+
+    public static int contarAcimaMedia(int[] vetor, double media) {
+        int contador = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] > media) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+}
