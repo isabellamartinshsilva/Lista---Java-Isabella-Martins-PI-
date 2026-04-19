b3237140b17b3bd30911df02525dbc56dc3f3f72
Isabella Martins <isabellamartinshsilva@gmail.com>
2026-04-19 09:33:04 -0300

    Q7.java

--- Q7.java
+++ Q7.java
@@ -1,8 +1,5 @@
 import java.util.Scanner;
-
-public class Q7 {
-
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int l = sc.nextInt();
